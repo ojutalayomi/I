495077bb6d841b947f9a63faf4f03da159b45888
Ayomide Ojutalayo <118499752+ojutalayomi@users.noreply.github.com>
2023-11-18 16:12:29 +0100

    backend_implementation

--- backend/static/js/clock.js
+++ backend/static/js/clock.js
@@ -1,3 +1,98 @@
+let che = document.querySelectorAll('#checkbox');
+let stats = document.querySelectorAll('img');
+let a = document.querySelectorAll('a');
+let maxi = document.querySelector('.maxi');
+let mini = document.querySelector('.mini');
+let sty = `
+  :root{
+  --darkmode: #000 !important;
+  --lightmode: #ffffff !important;
+  --dbodymode: #ffffff !important;
+  --lbodymode: #15202b !important;
+  }
+  
+ .navbar-light .navbar-brand {
+  color: #fff;
+}
+.navbar-light .navbar-nav .nav-link {
+  color: rgb(255 255 255) !important;
+}
+a.nav-item.nav-link {
+  color: #ffffff !important;
+}
+a.nav-item.nav-link.active {
+  color: #d1a103 !important;
+}
+.section-header p::before {
+  background: #ffffff !important;
+}
+.section-header p::after {
+  background: #ffffff !important;
+}
+.section-header p {
+  background: #15202b !important;
+}
+.section-header h2 {
+  color: #f2f2f2 !important;
+}
+.service .service-icon {
+  background: #15202b !important;
+}
+.service .service-text h3 {
+  color: #f2f2f2;
+}
+.service .service-text p {
+  color: #f2f2f2;
+}
+.service .service-item:hover {
+  box-shadow: inset 800px 0 0 0 #f2f2f2;
+}
+.service .service-item:hover .service-text h3,
+.service .service-item:hover .service-text p {
+    color: #15202b;
+}
+  `;
+  let sty1 = `
+  
+  `;  
+  let san = document.querySelector("style");
+  function staT(){
+    stats[4].setAttribute("src","https://github-readme-stats.vercel.app/api?username=ojutalayomi&amp;theme=dark&amp;hide_border=true&amp;include_all_commits=false&amp;count_private=false");
+    stats[5].setAttribute("src","https://github-readme-streak-stats.herokuapp.com/?user=ojutalayomi&amp;theme=dark&amp;hide_border=true");
+    stats[6].setAttribute("src","https://github-readme-stats.vercel.app/api/top-langs/?username=ojutalayomi&amp;theme=dark&amp;hide_border=true&amp;include_all_commits=false&amp;count_private=false&amp;layout=compact");
+  }
+
+che[0].addEventListener('click',function (){
+  if(che[0].checked == true){
+  san.innerHTML = sty;
+  staT();
+  } else if(che[0].checked == false) {
+    san.innerHTML = "";
+  }
+})
+
+che[1].addEventListener('click',function (){
+  if(che[1].checked == true){
+  san.innerHTML = sty;
+  staT();
+  } else if(che[1].checked == false) {
+    san.innerHTML = "";
+  }
+})
+setInterval(() => {
+if(window.screen.width < 1063){
+  maxi.style.display = "none";
+  mini.style.display ="block";
+} else {
+  mini.style.display ="none";
+  maxi.style.display = "block";
+} 
+  
+}, 1000);
+//che.onclick = () => {
+
+//}
+
 // Analog Clock
 var secondHand = document.querySelector(".second-hand");
 var minsHand = document.querySelector(".min-hand");
